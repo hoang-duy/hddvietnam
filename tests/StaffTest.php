ae9f46726f4796fd86278019b1e27e9ee65571c5
unknown <hoang-duy>
2023-06-11 16:17:51 +0700

    Fix Arch11112

--- tests/StaffTest.php
+++ tests/StaffTest.php
@@ -27,7 +27,7 @@ class StaffTest extends TestCase
     public function only_first_name()
     {
         $staff = new Staff;
-        $staff->first_name = "Ach11";
+        $staff->first_name = "Ach1112";
         $this->assertEquals('AAA', $staff->getFullName());
     }
 }
